77dc62a2a1c38a571232b32cdad17a95975b9183
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-02-06 18:04:09 +0100

    the function for processing is good! moving onto step4now

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 "use strict";
-
 // firstly, define all the input and outpul fields
+let result;
+
 let input = document.getElementById("input-box");
+
 let output = document.getElementById("output-box");
-// define all the dropdown options
-// define the button
+
+let dropdown = document.getElementById("dropdown-choice");
+
 const button = document.querySelector("#button");
 
-// event listener on button: on click it returns the result
-//output: return the result of the input and dropdown.value directed
+// Event listener on button: on "click" it returns the result.
 
 button.addEventListener("click", processOutput);
 
+// Which calls a processing function for output
+
 function processOutput() {
-  console.log("button was clicked, processing output");
-  /*let's read what was written in the input field */
-  console.log("input was this:", input.value);
+  // console.log("input was this:", input.value);
+  dropdown = document.getElementById("dropdown-choice").value;
+  input = document.getElementById("input-box").value;
 
-  // When you click the button,
-  // it processes whatever is in the inputbox
-  // and writes the result in the output box
+  // now, we need to compare the dropdown option values (choice1-8)
+  // and declare what happens with the input value accordingly.
+  if (dropdown === "choice1") {
+    result = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+  } else if (dropdown === "choice2") {
+    result = input.split(" ")[0];
+  } else if (dropdown === "choice3") {
+    let firstname = input.split(" ")[0];
+    result = firstname.length + " letters";
+  } else if (dropdown === "choice4") {
+    console.log("middle name");
+  } else if (dropdown === "choice5") {
+    fileType();
+  } else if (dropdown === "choice6") {
+    console.log("password");
+  } else if (dropdown === "choice7") {
+    console.log("3rd character uppercase");
+  } else if (dropdown === "choice8") {
+    console.log("uppercase and something crazy");
+  }
+  // and now when we have the result, display it inside Output field!
+  // console.log(result);
 
-  showResult();
+  function fileType() {
+    let isPng = input.endsWith(".png");
+    let isJpg = input.endsWith(".jpg");
+    if (isPng || isJpg) {
+      result = "The filename is JPG or PNG!";
+    } else {
+      result = "The filename is not JPG or PNG";
+    }
+  }
+
+  output.value = result;
 }
-
-function showResult() {
-  // remove previous input information
-  // inputbox.value = " ";
-}
-
-/**Comments from the class
- * can use the dropdown from the calculator file
- * refer to the document we just used for finding necessary strings
- */
